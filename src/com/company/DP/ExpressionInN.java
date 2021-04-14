b42d19e88bc0fc7aec1a58237fdfd4ccc880ecae
HJLE <lhj29@arielnetworks.co.kr>
2021-04-14 13:49:35 +0900

    dp 로 푸는 방법 n 번 사용해서 나올 수 있는 모든 경우의 수를 Set<Integer>[] 구조체에 담은 후 number가 포함되는 가장 작은 index 값을 반환!

--- src/com/company/DP/ExpressionInN.java
+++ src/com/company/DP/ExpressionInN.java
@@ -1,6 +1,8 @@
 package com.company.DP;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ExpressionInN {
     // N만으로 4칙 연산을 사용하여 number를 만드는 숫자들 중
@@ -11,7 +13,7 @@ public class ExpressionInN {
     int target;
     int answer = 9;
 
-    public int solution(int N, int number) {
+    public int solution1(int N, int number) {
         num = N;
         target = number;
 
@@ -40,4 +42,87 @@ public class ExpressionInN {
             temp = temp * 10 + num;
         }
     }
+    // dp 의 기본 개념은
+    // set[1] 에는 N을 1회 사용해서 만들 수 있는 모든 경우의 수를 담는다.
+    // set[2] 에는 ...
+    // 같은 방식으로 구하면서 number값을 가장 먼저 갖게 되는 idx 를 리턴하면 된다.
+    int MAX = 9;
+    public int solution(int N, int number) {
+        int answer = -1;
+
+        Set<Integer>[] setArr = new Set[9];
+
+        int t = N;
+        for (int i=1; i<MAX; i++) {
+            setArr[i] = new HashSet<>();
+            setArr[i].add(t);
+            t = t * 10 + N;
+        }
+
+        for (int i=1; i<MAX; i++) {
+            for (int j=1; j < i; j++) {
+                for (Integer a : setArr[j]) {
+                    for (Integer b : setArr[i-j]) {
+                        setArr[i].add(a + b);
+                        setArr[i].add(a - b);
+                        setArr[i].add(b - a);
+                        setArr[i].add(a * b);
+                        if (b != 0)
+                            setArr[i].add(a / b);
+                        if (a != 0)
+                            setArr[i].add(b / a);
+                    }
+                }
+            }
+        }
+        for (int i=1; i<MAX; i++){
+            if (setArr[i].contains(number)) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+
+
+
+        public int solutionDP(int N, int number) {
+            int answer = -1;
+            Set<Integer>[] setArr = new Set[9];
+            int t = N;
+
+            for(int i = 1; i < 9; i++) {
+                setArr[i] = new HashSet<>();
+                setArr[i].add(t);
+                t = t * 10 + N;
+            }
+
+            for(int i = 1; i < 9; i++) {
+                for(int j = 1; j < i; j++) {
+                    for(Integer a : setArr[j]) {
+                        for(Integer b : setArr[i - j]) {
+                            setArr[i].add(a + b);
+                            setArr[i].add(a - b);
+                            setArr[i].add(b - a);
+                            setArr[i].add(a * b);
+                            if(b != 0) {
+                                setArr[i].add(a / b);
+                            }
+                            if(a != 0) {
+                                setArr[i].add(b / a);
+                            }
+                        }
+                    }
+                }
+            }
+            for(int i = 1; i < 9; i++) {
+                if(setArr[i].contains(number)) {
+                    answer = i;
+                    break;
+                }
+            }
+            return answer;
+        }
+
 }
