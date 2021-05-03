74da63c2bf0185dec87010cdf03c988856e63ace
HJLE <lhj29@arielnetworks.co.kr>
2021-05-03 10:31:50 +0900

    숫자를 ++ 해가며 풀어도 효율성 문제 통과 됨..ㅎ

--- src/com/company/NextBiggerNumber.java
+++ src/com/company/NextBiggerNumber.java
@@ -9,11 +9,54 @@ package com.company;
 // 자리수를 추가해야하는 경우..?
 
 public class NextBiggerNumber {
+//    public char[] getBigerBinary(char[] num) {
+//        for (int i=num.length-2; i>=0; i++) {
+//            if (num[i] == '0') {
+//                num[i]
+//            }
+//        }
+//    }
+    public boolean isneedadd(char[] num) {
+        int firstzero = num.length;
+        int lastone = 0;
+
+        for (int i=0; i< num.length; i++) {
+            if (num[i] == '0' && firstzero > i)
+                firstzero = i;
+            else
+                lastone = i;
+        }
+        return lastone > firstzero;
+    }
+    public int solution(int n) {
+        String binary = Integer.toBinaryString(n);
+        int cntone = 0;
+        for (char c : binary.toCharArray()) {
+            if (c == '1') cntone++;
+        }
+        int answer = n+1;
+        for (;answer<1000000; answer++) {
+            int cnt = 0;
+            for (char c : Integer.toBinaryString(answer).toCharArray()) {
+                if (c == '1') cnt++;
+            }
+            if (cnt == cntone) return answer;
+        }
+        return answer;
+    }
+
     int firstzero = 0;
     int lastzero = 0;
     int lastone = 0;
-    int cnt = 0;
+    int cntone = 0;
+    int cntzero = 0;
     public Boolean isNeedAdd(char[] num) {
+        firstzero = 0;
+        lastzero = 0;
+        lastone = 0;
+        cntone = 0;
+        cntzero = 0;
+
         // 첫번째는 무조건 1임!
         // 첫번째로 0이 나타나는 idx 와 마지막으로 1이 나타나는 idx 비교!
         firstzero = num.length;
@@ -22,12 +65,14 @@ public class NextBiggerNumber {
                 if (firstzero > i)
                     firstzero = i;
 
-                if (lastzero < i)
+                if (i < num.length-2)
                     lastzero = i;
+
+                cntzero++;
             }
             else {
                 lastone = i;
-                cnt++;
+                cntone++;
             }
         }
         // 첫번째 0 가 마지막 1보다 크면,
@@ -35,16 +80,16 @@ public class NextBiggerNumber {
         // 즉, 1 사이에 0이 존재하지 않으면!!
         return firstzero > lastone;
     }
-    public int solution(int n) {
-        char[] num = String.valueOf(n).toCharArray();
+    public int solution1(int n) {//110 -> 1001 len (3) + 1 - 2 - cntone(1개수) - 1(10에 들어간 1)
+        char[] num =Integer.toBinaryString(n).toCharArray();
 
         // 추가되어야 하는 0의 갯수 = 10 + 0이 num.length - 1의 갯수 - 1 개 다음 나머지 1 전부 몰빵
         if (isNeedAdd(num)) {
             String result = "10";
-            for (int i=0; i<num.length-cnt-1;i++)
+            for (int i=0; i<num.length - cntone;i++)
                 result += '0';
 
-            for (int i=0; i<cnt-1;i++) {
+            for (int i=0; i<cntone-1;i++) {
                 result += '1';
             }
             return Integer.parseInt(result,2);
@@ -53,17 +98,20 @@ public class NextBiggerNumber {
         // lastzero 자리에 0이 1로 바뀌고,
         // 나머지 0의 갯수만큼 1을 좌라락 배치한 후 1의 갯수만큼 1을 배치한다
 
-//        int cnt0 = 0;
-//        for (int j=lastzero+1; j<num.length; j++) {
-//            if (num[j] == '0') cnt0++;
-//        }
-//
-//        for (int j=0; j<cnt0+1; j++)
-//            if (i < binary.length)
-//                binary[i++] = '0';
-//
-//        for (;i<binary.length;i++)
-//            binary[i] = '1';
+        int i = lastzero;
+        num[i++] = '1';
+        int cnt0 = 0;
+
+        for (int j=i; j<num.length; j++) {
+            if (num[j] == '0') cnt0++;
+        }
+
+            for (int j=0; j<cnt0+1; j++)
+            if (i < num.length)
+                num[i++] = '0';
+
+        for (;i<num.length;i++)
+            num[i] = '1';
 
         return Integer.valueOf(String.valueOf(num),2);
     }
