1c834fdf18619022aa48a15a01e1cb3135733147
HJLE <lhj29@arielnetworks.co.kr>
2021-05-11 15:42:52 +0900

    게임 맵 최단거리 DFS x, y 의 좌표가 배열에서는 [y][x] 로 계산해야 되는 부분을 조심! dp 가 더 빠른줄 알았는데 그냥 dps 를 여러번 부르지 않고 다음진행 좌표를 QUEUE 에 넣어서 while 로 반복시킨다..

--- src/com/company/Level2/ShortestDistanceOfGameMap.java
+++ src/com/company/Level2/ShortestDistanceOfGameMap.java
@@ -3,46 +3,116 @@ package com.company.Level2;
 // 두 팀으로 나누어서 진행, 상대 팀 전영을 먼저 파괴하면 승리
 // 캐릭터는 1,1 상대팀은 5,5
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ShortestDistanceOfGameMap {
     int DISTANCE = Integer.MAX_VALUE;
     int[][] maps;
-    public void func(int x, int y, int distance) {
+    boolean[][] visited;
+
+    int[] dx = {1,0,-1,0};
+    int[] dy = {0,1,0,-1};
+
+    public void dp(int x, int y, int distance) {
+        if (DISTANCE <= distance || distance > maps.length * maps[0].length) return;
+
+        if (x == (maps[0].length -1) && y == (maps.length-1)) {
+            DISTANCE = Math.min(DISTANCE, distance);
+            return;
+        }
+
+        for (int i=0; i<dx.length; i++) {
+            int ix = x + dx[i];
+            int iy = y + dy[i];
+            if (ix >= 0 && ix < maps[0].length && iy >= 0 && iy < maps.length)
+            {
+                if (!visited[iy][ix] && maps[iy][ix] == 1) {
+                    visited[iy][ix] = true;
+                    maps[iy][ix] = distance + 1;
+                    dp(ix, iy, distance + 1);
+                    visited[iy][ix] = false;
+                }
+
+            }
+        }
+    }
+
+
+    public int dfs2 (int x, int y, int[][] maps) {
+        Queue<Node> que = new LinkedList<>();
+        que.offer(new Node(x, y, 1));
+        visited[x][y] = true;
+
+        while(!que.isEmpty()) {
+            Node node = que.poll();
+            if (node.x == maps[0].length - 1 && node.y == maps.length - 1) {
+                return node.cost;
+            }
+
+            for (int i=0; i<dx.length; i++) {
+                int ix = node.x + dx[i];
+                int iy = node.y + dy[i];
+                if (ix >= 0 && ix < maps[0].length && iy >= 0 && iy < maps.length) {
+                    if (!visited[iy][ix] && maps[iy][ix] == 1) {
+                        visited[iy][ix] = true;
+                        que.offer(new Node(ix, iy, node.cost + 1));
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+    public void dfs(int x, int y, int distance) {
         if (DISTANCE <= distance) return;
 
         if (x == (maps[0].length -1) && y == (maps.length-1)) {
             DISTANCE = Math.min(DISTANCE, distance);
             return;
         }
-        // 오른쪽 방향
-        if (x + 1 < maps[0].length && maps[y][x+1] == 1) {
-            maps[y][x+1] = 0;
-            func(x+1, y, distance+1);
-            maps[y][x+1] = 1;
+
+        for (int i=0; i<dx.length; i++) {
+            int ix = x + dx[i];
+            int iy = y + dy[i];
+            if (ix >= 0 && ix < maps[0].length && iy >= 0 && iy < maps.length) {
+                if (!visited[iy][ix] && maps[iy][ix] == 1) {
+                    visited[iy][ix] = true;
+                    dfs(ix, iy, distance + 1);
+                    visited[iy][ix] = false;
+                }
+            }
         }
-        // 아래쪽 방향
-        if (y + 1 < maps.length && maps[y+1][x] == 1) {
-            maps[y+1][x] = 0;
-            func(x, y+1, distance+1);
-            maps[y+1][x] = 1;
+    }
+    public void initVisited() {
+        visited = new boolean[maps.length][maps[0].length];
+
+        for (int i=0; i<maps.length; i++) {
+            for (int j=0; j<maps[0].length; j++) {
+                if (maps[i][j] == 0)
+                visited[i][j] = true;
+                else
+                visited[i][j] = false;
+            }
         }
-        // 왼쪽 방향
-        if (x - 1 >= 0 && maps[y][x-1] == 1) {
-            maps[y][x-1] = 0;
-            func(x-1, y, distance+1);
-            maps[y][x-1] = 1;
-        }
-        // 위쪽 방향
-        if (y - 1 >= 0 && maps[y-1][x] == 1) {
-            maps[y-1][x] = 0;
-            func(x, y-1, distance+1);
-            maps[y-1][x] = 1;
-        }
+        visited[0][0] = true;
     }
     public int solution(int[][] MAP) {
         int answer = 1;
         maps = MAP;
         maps[0][0] = 0;
-        func(0, 0, answer);
+        initVisited();
+        dfs2(0, 0, MAP);
         return DISTANCE == Integer.MAX_VALUE ? -1 : DISTANCE;
     }
+    public class Node {
+        int x;
+        int y;
+        int cost;
+        public Node(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
+        }
+    }
+
 }
